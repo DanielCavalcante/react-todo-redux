422e22c4426a8e05d27fb8dcfc0c0ffc44d9666c
Daniel Cavalcante <c0500272@vale.com>
2018-10-29 14:07:54 -0300

    Up project

---
+++ src/about/about.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import PageHeader from '../template/pageHeader'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+      </div>
+    );
+  }
+};
